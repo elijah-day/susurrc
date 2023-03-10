5eced7974a3e3cbcdb80ee180dd0d52d92bf4a66
Elijah <elijahday8482@gmail.com>
2023-03-10 12:19:29 -0600

    Add GUI method for connecting to servers

--- src/susurrc.c
+++ src/susurrc.c
@@ -22,12 +22,30 @@ SOFTWARE. */
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_net.h"
 #include "sodium.h"
+#include "src/err.h"
 #include "src/init.h"
 #include "src/net.h"
 #include "src/susurrc.h"
 #include "stdbool.h"
 #include "stdlib.h"
 
+static const char *HEADER_BAR_CONNECTED_TITLE = "Connected";
+static const char *HEADER_BAR_DISCONNECTED_TITLE = "Disconnected";
+static const char *MSG_SEND_ENTRY_PLACEHOLDER = "Send a message...";
+static const char *SERVER_CONNECT_BUTTON_LABEL = "Connect";
+
+static const char *SERVER_HOSTNAME_ENTRY_PLACEHOLDER =
+	"Hostname or IP address";
+	
+static const char *SERVER_PORT_ENTRY_PLACEHOLDER = "Port";
+static const char *WINDOW_TITLE = "SusurrC";
+static const int BOX_PACK_PADDING = 2;
+static const int BOX_SPACING = 4;
+static const int DEFAULT_WINDOW_HEIGHT = 360;
+static const int DEFAULT_WINDOW_WIDTH = 640;
+static const int SOCKET_CHECK_TIMEOUT = 1;
+
+static char msg_recv_buf[MAX_MSG_LEN * MAX_MSG_CNT];
 static int ready_socket_cnt;
 static IPaddress server_ip;
 static msg_data_t msg_data;
@@ -36,58 +54,125 @@ static TCPsocket server_socket;
 static unsigned char privkey[crypto_box_SECRETKEYBYTES];
 static unsigned char pubkey[crypto_box_PUBLICKEYBYTES];
 
-static bool init_client(int argc, char *argv[])
-{	
-	bool init_success = false;
-
-	crypto_box_keypair(pubkey, privkey);
-	
-	init_success = setup_server_connection
-	(
-		&server_ip,
-		&server_socket,
-		argv[1],
-		atoi(argv[2])
-	);
-	
-	if(init_success)
-		init_success = init_client_socket_set(&socket_set, &server_socket);
-}
-
-static void terminate_client(void)
-{
-	if(server_socket && socket_set)
-		SDLNet_TCP_DelSocket(socket_set, server_socket);
-	
-	SDLNet_TCP_Close(server_socket);
-	server_socket = NULL;
-	
-	if(socket_set)
-		SDLNet_FreeSocketSet(socket_set);
-}
-
-static const char *WINDOW_TITLE = "SusurrC";
-static const int BOX_PACK_PADDING = 2;
-static const int BOX_SPACING = 4;
-static const int DEFAULT_WINDOW_HEIGHT = 360;
-static const int DEFAULT_WINDOW_WIDTH = 640;
-static const int SOCKET_CHECK_TIMEOUT = 1;
-
-static GtkEntryBuffer *msg_send_text_buffer;
-static char msg_recv_buf[MAX_MSG_LEN * MAX_MSG_CNT];
+static GtkEntryBuffer *msg_send_entry_buffer;
+static GtkEntryBuffer *server_hostname_entry_buffer;
+static GtkEntryBuffer *server_port_entry_buffer;
 static GtkTextBuffer *msg_recv_text_buffer;
-static GtkTextBuffer *user_list_text_buffer;
+static GtkWidget *control_box;
 static GtkWidget *header_bar;
 static GtkWidget *inner_box;
 static GtkWidget *msg_box;
 static GtkWidget *msg_recv_text_view;
 static GtkWidget *msg_send_entry;
 static GtkWidget *outer_box;
-static GtkWidget *user_list_text_view;
+static GtkWidget *server_connect_button;
+static GtkWidget *server_hostname_entry;
+static GtkWidget *server_port_entry;
 static GtkWidget *window;
+static guint recv_msg_from_server_id;
 
-static void send_client_msg(GtkWidget *msg_send_entry, gpointer data)
+static void append_to_msg_recv_buffer(const char *msg)
 {
+	/* Append the new message to the received message buffer */
+	strcat(msg_recv_buf, msg);
+	strcat(msg_recv_buf, "\n");
+	
+	/* Update text view with the new received message buffer */
+	gtk_text_buffer_set_text
+	(
+		GTK_TEXT_BUFFER(msg_recv_text_buffer),
+		msg_recv_buf,
+		-1
+	);
+}
+
+static void set_header_bar_title_and_subtitle
+(
+	const char *title,
+	const char *subtitle
+)
+{
+	gtk_header_bar_set_title(GTK_HEADER_BAR(header_bar), title);
+	gtk_header_bar_set_subtitle(GTK_HEADER_BAR(header_bar), subtitle);
+}
+
+static void terminate_socket_connection(void)
+{
+	if(server_socket && socket_set)
+		SDLNet_TCP_DelSocket(socket_set, server_socket);
+		
+	if(server_socket)
+		SDLNet_TCP_Close(server_socket);
+	
+	if(socket_set)
+		SDLNet_FreeSocketSet(socket_set);
+		
+	server_socket = NULL;
+	socket_set = NULL;
+		
+	/* Possible TODO: Move the header bar setups from the socket
+	connection/termination functions.  They aren't related to socket
+	connections (Though, they make sense here logically based on when each
+	function is called, since the header is going to be set
+	simultaneously) */
+	set_header_bar_title_and_subtitle
+	(
+		HEADER_BAR_DISCONNECTED_TITLE,
+		""
+	);
+}
+
+static bool init_socket_connection
+(
+	const char *server_hostname,
+	int server_port
+)
+{	
+	bool init_success;
+
+	/* Terminate any active connections beforehand so the client doesn't
+	accidentally fill up unnecessary slots */
+	terminate_socket_connection();
+	
+	/* Setup the socket connection */
+	init_success = setup_server_connection
+	(
+		&server_ip,
+		&server_socket,
+		server_hostname,
+		server_port
+	);
+	
+	/* Check if the connection could be made */
+	if(init_success)
+		init_success = init_client_socket_set(&socket_set, &server_socket);
+	
+	/* Setup the header on success */
+	if(init_success)
+		set_header_bar_title_and_subtitle
+		(
+			HEADER_BAR_CONNECTED_TITLE,
+			server_hostname
+		);
+	
+	/* Return */
+	return init_success;
+}
+
+static void send_msg_to_server(GtkWidget *msg_send_entry, gpointer data)
+{
+	/* Avoid trying to send a message while there are no socket connections */
+	if(server_socket == NULL)
+	{
+		print_err
+		(
+			"send_msg_to_server",
+			"Could not send message.  No active connection"
+		);
+		
+		return;
+	}
+
 	/* Send the message to the server */	
 	char msg[MAX_MSG_LEN];
 	strcpy(msg, gtk_entry_get_text(GTK_ENTRY(msg_send_entry)));
@@ -105,7 +190,7 @@ static void send_client_msg(GtkWidget *msg_send_entry, gpointer data)
 	gtk_entry_set_text(GTK_ENTRY(msg_send_entry), "");
 }
 
-static gboolean recv_client_msg(gpointer data)
+static gboolean recv_msg_from_server(gpointer data)
 {
 	/* Check for socket activity */
 	ready_socket_cnt = SDLNet_CheckSockets(socket_set, SOCKET_CHECK_TIMEOUT);
@@ -117,7 +202,7 @@ static gboolean recv_client_msg(gpointer data)
 			/* Receive the message */
 			char msg[MAX_MSG_LEN];
 		
-			recv_msg
+			bool recv_success = recv_msg
 			(
 				msg,
 				&msg_data,
@@ -128,22 +213,64 @@ static gboolean recv_client_msg(gpointer data)
 			
 			if(strcmp(msg, "") != 0)
 			{
-				/* Append the new message to the received message buffer */
-				strcat(msg_recv_buf, msg);
-				strcat(msg_recv_buf, "\n");
-				
-				/* Update text view with the new received message buffer */
-				gtk_text_buffer_set_text
-				(
-					GTK_TEXT_BUFFER(msg_recv_text_buffer),
-					msg_recv_buf,
-					-1
-				);
+				append_to_msg_recv_buffer(msg);
+			}
+			
+			if(recv_success == false)
+			{
+				/* Close the connection and return false so that GLib stops
+				attempting to receive messages */
+				terminate_socket_connection();
+				return FALSE;
 			}
 		}
 		
 	/* Return true so GLib doesn't remove this fucntion from the main loop */
 	return TRUE;
+}
+
+static void connect_to_server(gpointer data)
+{
+	/* Get the hostname and port */
+	const char *server_hostname =
+		gtk_entry_buffer_get_text(GTK_ENTRY_BUFFER
+		(server_hostname_entry_buffer));
+	
+	const int server_port =
+		atoi(gtk_entry_buffer_get_text(GTK_ENTRY_BUFFER
+		(server_port_entry_buffer)));
+
+	/* Attempt to make the socket connection */
+	bool init_success = init_socket_connection
+	(
+		server_hostname,
+		server_port
+	);
+	
+	if(init_success)
+	{
+		/* Generate the client's keypair */
+		crypto_box_keypair(pubkey, privkey);
+		
+		/* Add message receiving to the GLib event loop */
+		recv_msg_from_server_id = g_idle_add
+		(
+			(void *)recv_msg_from_server,
+			NULL
+		);
+	}
+	else
+	{
+		print_err("connect_to_server", "Could not connect to server");
+		
+		/* Remove message receiving from the GLib event loop.  Otherwise,
+		it will try to receive messages on the terminated socket */
+		if(recv_msg_from_server_id != 0)
+			g_source_remove(recv_msg_from_server_id);
+		
+		/* Terminate the socket connection */
+		terminate_socket_connection();
+	}
 }
 
 static void setup_widgets(void)
@@ -189,27 +316,79 @@ static void setup_widgets(void)
 		BOX_PACK_PADDING
 	);
 	
-	/* user_list_text_view */
-	user_list_text_view = gtk_text_view_new();
-	
-	user_list_text_buffer =
-		gtk_text_view_get_buffer(GTK_TEXT_VIEW(user_list_text_view));
+	/* control_box */
+	control_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, BOX_SPACING);
 	
 	gtk_box_pack_start
 	(
 		GTK_BOX(inner_box),
-		user_list_text_view,
+		control_box,
 		TRUE,
 		TRUE,
 		BOX_PACK_PADDING
 	);
 	
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(user_list_text_view), FALSE);
+	/* server_hostname_entry */
+	server_hostname_entry = gtk_entry_new();
 	
-	gtk_text_view_set_wrap_mode
+	server_hostname_entry_buffer =
+		gtk_entry_get_buffer(GTK_ENTRY(server_hostname_entry));
+	
+	gtk_entry_set_placeholder_text
 	(
-		GTK_TEXT_VIEW(user_list_text_view),
-		GTK_WRAP_WORD_CHAR
+		GTK_ENTRY(server_hostname_entry),
+		SERVER_HOSTNAME_ENTRY_PLACEHOLDER
+	);
+	
+	gtk_box_pack_start
+	(
+		GTK_BOX(control_box),
+		server_hostname_entry,
+		FALSE,
+		FALSE,
+		BOX_PACK_PADDING
+	);
+	
+	/* server_port_entry */
+	server_port_entry = gtk_entry_new();
+	
+	server_port_entry_buffer =
+		gtk_entry_get_buffer(GTK_ENTRY(server_port_entry));
+		
+	gtk_entry_set_placeholder_text
+	(
+		GTK_ENTRY(server_port_entry),
+		SERVER_PORT_ENTRY_PLACEHOLDER
+	);
+	
+	gtk_box_pack_start
+	(
+		GTK_BOX(control_box),
+		server_port_entry,
+		FALSE,
+		FALSE,
+		BOX_PACK_PADDING
+	);
+	
+	/* server_connect_button */
+	server_connect_button =
+		gtk_button_new_with_label(SERVER_CONNECT_BUTTON_LABEL);
+	
+	gtk_box_pack_start
+	(
+		GTK_BOX(control_box),
+		server_connect_button,
+		FALSE,
+		FALSE,
+		BOX_PACK_PADDING
+	);
+	
+	g_signal_connect
+	(
+		server_connect_button,
+		"clicked",
+		G_CALLBACK(connect_to_server),
+		NULL
 	);
 	
 	/* msg_box */
@@ -250,8 +429,16 @@ static void setup_widgets(void)
 	/* msg_send_entry */
 	msg_send_entry = gtk_entry_new();
 	
-	msg_send_text_buffer =
+	msg_send_entry_buffer =
 		gtk_entry_get_buffer(GTK_ENTRY(msg_send_entry));
+	
+	gtk_entry_set_placeholder_text
+	(
+		GTK_ENTRY(msg_send_entry),
+		MSG_SEND_ENTRY_PLACEHOLDER
+	);
+	
+	g_signal_connect(msg_send_entry, "activate", G_CALLBACK(send_msg_to_server), NULL);
 	
 	gtk_box_pack_start
 	(
@@ -261,8 +448,6 @@ static void setup_widgets(void)
 		FALSE,
 		BOX_PACK_PADDING
 	);
-	
-	g_signal_connect(msg_send_entry, "activate", G_CALLBACK(send_client_msg), NULL);
 }
 
 int main(int argc, char *argv[])
@@ -272,18 +457,23 @@ int main(int argc, char *argv[])
 	init_success = init_libsdl();
 	init_success = init_libsdlnet();
 	init_success = init_libsodium();
-	init_success = init_client(argc, argv);
+	
+	server_socket = NULL;
+	socket_set = NULL;
 	
 	if(init_success)
 	{
-		gtk_init(NULL, NULL);
+		gtk_init(&argc, &argv);
 		setup_widgets();
 		gtk_widget_show_all(window);
-		g_idle_add((void *)recv_client_msg, NULL);
+		
+		/* Called here to set the header bar to "disconnected" */
+		terminate_socket_connection();
+		
 		gtk_main();
 	}
 	
-	terminate_client();
+	terminate_socket_connection();
 	SDLNet_Quit();
 	SDL_Quit();
 	
